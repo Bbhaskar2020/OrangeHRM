6765314d8c838d3e1684b3d4a49d4b5e43b18d52
Bbhaskar2020 <bbhaskarjyotiphukan@yahoo.com>
2025-08-09 13:59:22 +0530

    Changes added

---
+++ locators_xpath/locators.py
@@ -0,0 +1,8 @@
+
+class login_page:
+    user_name = '//input[@name='username']'
+    password = '//input[@name='password']'
+    forgot_password = '//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']'
+    page_title = '//title'
+    login_button = '//button[@type='submit']'
+
---
+++ utils/base_page.py
@@ -0,0 +1,13 @@
+class BasePage:
+    def __init__(self, page):
+        self.page = page
+
+    def click(self, selector):
+        self.page.click(selector)
+
+    def fill(self, selector, value):
+        self.page.fill(selector, value)
+
+    def get_text(self, selector):
+        return self.page.text_content(selector)
+
