d7f8ba12180c3d2f11535203af4b0061ee939390
Bbhaskar2020 <bbhaskarjyotiphukan@yahoo.com>
2025-09-10 15:20:44 +0530

    Delete locators_xpath directory

--- locators_xpath/locators.py
+++
@@ -1,8 +0,0 @@
-
-class login_page:
-    user_name = '//input[@name='username']'
-    password = '//input[@name='password']'
-    forgot_password = '//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']'
-    page_title = '//title'
-    login_button = '//button[@type='submit']'
-
